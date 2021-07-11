48b5dac5d50c35aed4a57eb3a0456dae515c5632
k0TAR <kazuki.tada@fj.ics.keio.ac.jp>
2021-07-11 14:09:13 +0900

    Made generic and utilized GetAssetFromAssetBundle.

---
+++ Assets/Scripts/AssetBundleGet/DrawTextures.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawTextures : MonoBehaviour
+{
+    [SerializeField] private RawImage _img = null;
+
+    private async void Start()
+    {
+        Texture[] result = null;
+
+        StartCoroutine(
+            GetAssetFromAssetBundle.GetAssetsAsync<Texture>(
+                "AssetBundles/StandaloneWindows64/lightfield",
+                (r) => result = r
+            )
+        );
+        
+
+        _img.texture = await Task.Run(() => {
+            while (result == null) ;
+            return result[0];
+        }); ;
+    }
+}
---
+++ Assets/Scripts/AssetBundleGet/GetAssetFromAssetBundle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using System;
+
+public static class GetAssetFromAssetBundle
+{
+    private static string _asset_bundle_path = Application.streamingAssetsPath + "/";
+
+    public static IEnumerator GetAssetsAsync<T>(string path, Action<T[]> Callback)
+    {
+        _asset_bundle_path += path;
+        Debug.Log("START GETTING ASSETS.");
+        
+        AssetBundleCreateRequest loadFileAsync = AssetBundle.LoadFromFileAsync(_asset_bundle_path);
+        while (!loadFileAsync.isDone)
+        {
+            Debug.Log("LoadFromFileAsync progress: " + (loadFileAsync.progress * 100.0f) + "%");
+            yield return null;
+        }
+        AssetBundle assetBundle = loadFileAsync.assetBundle;
+        AssetBundleRequest loadAssetAsync = assetBundle.LoadAllAssetsAsync<T>();
+        while (!loadAssetAsync.isDone)
+        {
+            Debug.Log("LoadAllAssetsAsync progress: " + (loadAssetAsync.progress * 100.0f) + "%");
+            yield return null;
+        }
+        T[] assets = loadAssetAsync.allAssets.Cast<T>().ToArray();
+
+        Callback(assets);
+        Debug.Log("GETTING ASSETS END.");
+    }
+
+    public static void GetAssets<T>(string path, Action<T[]> Callback)
+    {
+
+    }
+}
