4c9662cdac41c5bcf865cfddd0b0afffac61525c
k0TAR <kazuki.tada@fj.ics.keio.ac.jp>
2021-07-13 00:55:09 +0900

    Get Light Field positions.
    regex

--- Assets/Scripts/AssetBundleGet/DrawTextures.cs
+++ Assets/Scripts/AssetBundleGet/DrawTextures.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,8 +9,13 @@ using UnityEngine.UI;
 public class DrawTextures : MonoBehaviour
 {
     [SerializeField] private RawImage _img = null;
+    private const int uvCountSide = 17;
 
-    private async void Start()
+    private Vector2[,] uvs = new Vector2[uvCountSide, uvCountSide];
+    private Texture[,] textures;
+
+
+    private async void Awake()
     {
         Texture[] result = null;
 
@@ -20,9 +27,26 @@ public class DrawTextures : MonoBehaviour
         );
         
 
-        _img.texture = await Task.Run(() => {
+        await Task.Run(() => {
             while (result == null);
-            return result[0];
-        }); ;
+        });
+
+
+
+        for(int i = 0; i < uvCountSide; i++)
+        {
+            for(int j = 0; j < uvCountSide; j++)
+            {
+                var tex = result[i * uvCountSide + j];
+                var numberArray = Regex.Matches(tex.name, @"[+-]?\d+(?:\.\d+)?")
+                .Cast<Match>()
+                .Select(m => float.Parse(m.Value))
+                .ToArray();
+
+                uvs[i,j] = new Vector2(numberArray[2], numberArray[3]);
+                Debug.Log(uvs[i,j]);
+            }
+        }
+
     }
 }
