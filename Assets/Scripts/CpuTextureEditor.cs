357ef12559c32892e58bd0e2294ef535564513e4
k0TAR <kazuki.tada@fj.ics.keio.ac.jp>
2021-08-18 19:20:58 +0900

    CPU Texture Edtior.

---
+++ Assets/Scripts/CpuTextureEditor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CpuTextureEditor : MonoBehaviour
+{
+    [SerializeField] RawImage _beforeImage = null;
+    [SerializeField] RawImage _afterImage = null;
+
+    private Texture2D _input;
+    private Texture2D _output;
+
+}
