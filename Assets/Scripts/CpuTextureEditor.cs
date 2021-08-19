9377128764034bdc1f09099efa24952f2249fe89
k0TAR <kazuki.tada@fj.ics.keio.ac.jp>
2021-08-19 15:36:23 +0900

    Implemented CpuTextureEditor.
    Pass algorithm for each pixel to CpuTextureEditor to calculate all the pixels.

--- Assets/Scripts/CpuTextureEditor.cs
+++ Assets/Scripts/CpuTextureEditor.cs
@@ -1,14 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class CpuTextureEditor : MonoBehaviour
+public static class CpuTextureEditor
 {
-    [SerializeField] RawImage _beforeImage = null;
-    [SerializeField] RawImage _afterImage = null;
+    /// <summary>
+    /// calculates the passed algorithm for each pixels in the texture on CPU.
+    /// </summary>
+    /// <param name="inputTexture">Input texture.</param>
+    /// <param name="algorithm">The algorithm that will be applied. <br />
+    /// Algorithm takes a texture (Texture2D), applying pixel coordinate (int2).</param>
+    /// <returns>The result of the texture.</returns>
+    public static Texture2D CalculateEachPixel(Texture2D inputTexture, System.Func<Texture2D, Vector2Int, Color> algorithm)
+    {
+        Texture2D result = ToTexture2D(GenerateRenderTexture(inputTexture));
 
-    private Texture2D _input;
-    private Texture2D _output;
+        int width = inputTexture.width;
+        int height = inputTexture.height;
 
+        Vector2Int coord = new Vector2Int(0, 0);
+        for (; coord.x < width; coord += Vector2Int.right)
+        {
+            for(; coord.y < height; coord += Vector2Int.up)
+            {
+                Color settingColor = algorithm(inputTexture, coord);
+                result.SetPixel(coord.x, coord.y, settingColor);
+            }
+        }
+
+        result.Apply();
+        return result;
+    }
+
+
+
+    public static RenderTexture GenerateRenderTexture(Texture input)
+    {
+        var result = new RenderTexture(input.width, input.height, 0, RenderTextureFormat.ARGB32);
+        result.enableRandomWrite = true;
+        result.filterMode = FilterMode.Point;
+        result.Create();
+        return result;
+    }
+
+    public static Texture2D ToTexture2D(RenderTexture rTex)
+    {
+        Texture2D tex = new Texture2D(512, 512, TextureFormat.RGB24, false);
+        
+        // Remember currently active render texture
+        RenderTexture currentActiveRT = RenderTexture.active;
+        // ReadPixels looks at the active RenderTexture.
+        RenderTexture.active = rTex;
+        tex.ReadPixels(new Rect(0, 0, rTex.width, rTex.height), 0, 0);
+        tex.Apply();
+
+        RenderTexture.active = currentActiveRT;
+        return tex;
+    }
 }
