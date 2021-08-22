0f8fc4765bec0eb158bece171d09e9375200e442
k0TAR <kazuki.tada@fj.ics.keio.ac.jp>
2021-08-22 15:14:53 +0900

    Sobel Gradient Map implemented. 1-0.5-2.

--- Assets/Scripts/MorphologicalTransformations/Thinning2OnePixel.cs
+++ Assets/Scripts/MorphologicalTransformations/Thinning2OnePixel.cs
@@ -31,9 +31,9 @@ public class Thinning2OnePixel : MonoBehaviour
         Dictionary<string, object> computeShaderParams = new Dictionary<string, object>();
         computeShaderParams.Add("AlphaOn", Convert.ToInt32(_alphaOn));
 
-        Debug.Log("===Thinning2OnePixel on GPU START===");
-        System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
-        sw.Start();
+        //Debug.Log("===Thinning2OnePixel on GPU START===");
+        //System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+        //sw.Start();
 
         var result = ComputeShaderApplier.RunComputeShader(_computeShader, _beforeImage.texture, computeShaderParams); ;
         for (int i = 1; i < _thinningIteration; i++)
@@ -43,7 +43,7 @@ public class Thinning2OnePixel : MonoBehaviour
 
         _afterImage.texture = result;
 
-        sw.Stop();
-        Debug.Log("Iteration: " + _thinningIteration + ", Processing Time: " + sw.ElapsedMilliseconds + " ms");
+        //sw.Stop();
+        //Debug.Log("Iteration: " + _thinningIteration + ", Processing Time: " + sw.ElapsedMilliseconds + " ms");
     }
 }
---
+++ Assets/Scripts/SobelGradMap.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SobelGradMap : MonoBehaviour
+{
+    [SerializeField] RawImage _beforeImage = null;
+    [SerializeField] RawImage _afterImage = null;
+
+    [SerializeField] private ComputeShader _computeShader = null;
+    [SerializeField] private bool _alphaOn = false;
+
+
+    private void Start()
+    {
+        if (!SystemInfo.supportsComputeShaders)
+        {
+            Debug.LogError("Does not support Compute Shader.");
+            return;
+        }
+
+    }
+
+    private void OnValidate()
+    {
+        if (!ComputeShaderApplier.IsInitializationEnough(ref _beforeImage, ref _afterImage, this)) return;
+
+        var result = ComputeShaderApplier.RunComputeShader(_computeShader, _beforeImage.texture); ;
+
+        _afterImage.texture = result;
+
+
+    }
+
+}
